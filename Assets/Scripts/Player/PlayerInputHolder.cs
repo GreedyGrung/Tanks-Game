eb3eeb59e1d8df6bb1ebdce8080403a778bb2bb6
Denis Litvinets <denis.litvinets@gmail.com>
2023-06-30 01:48:55 +0300

    implement tank tower rotation

--- Assets/Scripts/Player/PlayerInputHolder.cs
+++ Assets/Scripts/Player/PlayerInputHolder.cs
@@ -1,14 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerInputHolder : MonoBehaviour
 {
     public Vector2 MovementInput { get; private set; }
+    public Vector2 MousePosition { get; private set; }
 
     public void OnMoveInput(InputAction.CallbackContext context)
     {
         MovementInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnMouseInput(InputAction.CallbackContext context)
+    {
+        MousePosition = context.ReadValue<Vector2>();
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,26 +2,53 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Tank Body")]
     [SerializeField] private float _moveSpeed = 2f;
-    [SerializeField] private float _rotationSpeed = 90f;
+    [SerializeField] private float _bodyRotationSpeed = 90f;
+
+    [Header("Tank Tower")]
+    [SerializeField] private Transform _tankTower;
+    [SerializeField] private float _towerRotationSpeed = 90f;
 
     private PlayerInputHolder _playerInputHolder;
-    private int _rotationInverseCoefficient;
+    private Camera _mainCamera;
+    private Vector2 _mousePosition;
+    Quaternion _targetRotation;
+    private int _bodyRotationInverseCoefficient;
 
     private void Start()
     {
         _playerInputHolder = GetComponent<PlayerInputHolder>();
+        _mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        CalculateTowerRotationAngle();
     }
 
     private void FixedUpdate()
     {
-        HandleMovement();
+        HandleBodyMovement();
+        HandleTowerRotation();
     }
 
-    private void HandleMovement()
+    private void HandleBodyMovement()
     {
         transform.Translate(Vector3.up * _playerInputHolder.MovementInput.y * _moveSpeed * Time.deltaTime);
-        _rotationInverseCoefficient = _playerInputHolder.MovementInput.y < 0 ? -1 : 1;
-        transform.Rotate(Vector3.forward * -_playerInputHolder.MovementInput.x * _rotationInverseCoefficient * _rotationSpeed * Time.deltaTime);
+        _bodyRotationInverseCoefficient = _playerInputHolder.MovementInput.y < 0 ? -1 : 1;
+        transform.Rotate(Vector3.forward * -_playerInputHolder.MovementInput.x * _bodyRotationInverseCoefficient * _bodyRotationSpeed * Time.deltaTime);
+    }
+
+    private void CalculateTowerRotationAngle()
+    {
+        _mousePosition = _mainCamera.ScreenToWorldPoint(_playerInputHolder.MousePosition);
+        _targetRotation = Quaternion.LookRotation(Vector3.forward, _mousePosition - (Vector2)_tankTower.position);
+    }
+
+    private void HandleTowerRotation()
+    {
+        float rotationStep = _towerRotationSpeed * Time.deltaTime;
+        _tankTower.transform.rotation = Quaternion.RotateTowards(_tankTower.transform.rotation, _targetRotation, rotationStep);
     }
 }
