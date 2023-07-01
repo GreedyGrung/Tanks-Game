c0ac6cb50d7143e1fa5a9ad5a60125cbb1251eff
Denis Litvinets <denis.litvinets@gmail.com>
2023-07-01 20:56:11 +0300

    little refactoring

--- Assets/Scripts/Object Pool/BaseProjectilePool.cs
+++ Assets/Scripts/Object Pool/BaseProjectilePool.cs
@@ -2,5 +2,10 @@ using UnityEngine;
 
 public abstract class BaseProjectilePool : MonoBehaviour
 {
+    [SerializeField] private int _poolSize;
+    [SerializeField] private bool _autoExpand;
+
     public ObjectPool<Projectile> Pool { get; protected set; }
+    public int PoolSize => _poolSize;
+    public bool AutoExpand => _autoExpand;
 }
--- Assets/Scripts/Object Pool/HighExplosiveProjectilePool.cs
+++ Assets/Scripts/Object Pool/HighExplosiveProjectilePool.cs
@@ -3,12 +3,10 @@ using UnityEngine;
 public class HighExplosiveProjectilePool : BaseProjectilePool
 {
     [SerializeField] private HighExplosiveProjectile _projectile;
-    [SerializeField] private int _poolSize = 20;
-    [SerializeField] private bool _autoExpand = false;
 
     private void Start()
     {
-        Pool = new(_projectile, _poolSize, transform);
-        Pool.AutoExpand = _autoExpand;
+        Pool = new(_projectile, PoolSize, transform);
+        Pool.AutoExpand = AutoExpand;
     }
 }
