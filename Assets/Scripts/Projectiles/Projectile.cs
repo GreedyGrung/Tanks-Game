dcf6c8e8c30b7c65cb3d7f87d2db4192e8f3362b
Denis Litvinets <denis.litvinets@gmail.com>
2023-07-04 18:36:46 +0300

    add projectiles explosions

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -5,11 +5,19 @@ public abstract class Projectile : MonoBehaviour
     [SerializeField] private float _moveSpeed = 10;
     [SerializeField] private float _damage;
     [SerializeField] private float _lifetime;
+    [SerializeField] private ProjectileAnimation _projectileAnimation;
+    [SerializeField] private GameObject _visuals;
 
     private float _timeFromSpawn;
+    private bool _exploded;
 
     public virtual void Update()
     {
+        if (_exploded)
+        {
+            return;
+        }
+
         transform.Translate(Vector3.right * _moveSpeed * Time.deltaTime);
         _timeFromSpawn += Time.deltaTime;
 
@@ -22,17 +30,21 @@ public abstract class Projectile : MonoBehaviour
     private void OnDisable()
     {
         _timeFromSpawn = 0f;
+        _exploded = false;
+        _visuals.SetActive(true);
     }
 
     public abstract void Explode();
 
     public virtual void OnTriggerEnter2D(Collider2D collision)
     {
+        _projectileAnimation.gameObject.SetActive(true);
+        _exploded = true;
+        _visuals.SetActive(false);
+
         if (collision.TryGetComponent(out IDamageable damageable))
         {
             damageable.TakeDamage(_damage);
         }
-
-        gameObject.SetActive(false);
     }
 }
---
+++ Assets/Scripts/Projectiles/ProjectileAnimation.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class ProjectileAnimation : MonoBehaviour
+{
+    private Animator _animator;
+    private Projectile _projectile;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _projectile = GetComponentInParent<Projectile>();
+    }
+
+    private void OnEnable()
+    {
+        PlayAnimation();
+    }
+
+    public void PlayAnimation()
+    {
+        _animator.enabled = true;
+    }
+
+    public virtual void DisableObject()
+    {
+        _animator.enabled = false;
+        gameObject.SetActive(false);
+        _projectile.gameObject.SetActive(false);
+    }
+}
